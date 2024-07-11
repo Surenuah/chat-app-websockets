49e693cce26032e6b001d70dbbb82fbd5ae41a71
aarstanbekov <aarstanbekov@innoforce.kz>
2024-07-11 19:43:47 +0500

    feat: added login endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,8 @@
-import { createUser, checkUsernameExists } from '../services/user.service.js';
+import {
+	createUser,
+	checkUsernameExists,
+	validatePassword,
+} from '../services/user.service.js';
 import { generateTokenAndSetCookie } from '../utils/generateToken.js';
 
 export const signupUser = async (req, res) => {
@@ -34,5 +38,31 @@ export const signupUser = async (req, res) => {
 	}
 };
 
-export const loginUser = (req, res) => console.log('login user');
+export const loginUser = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await checkUsernameExists(username);
+		const isPasswordCorrect = await validatePassword(
+			password,
+			user?.password || ''
+		);
+
+		if (!user || !isPasswordCorrect) {
+			return res.status(400).json({ error: 'Invalid username or password' });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePicture: user.profilePicture,
+		});
+	} catch (error) {
+		console.log('Error in login controller', error.message);
+		res.status(500).json({ error: error.message });
+	}
+};
+
 export const logoutUser = (req, res) => console.log('logout user');
